63f5bf81652b08b0c24c44783f50780b2f24749e
sandhyas2308 <23104140@rmd.ac.in>
2026-06-29 16:27:42 +0530

    Added Exercise 1 - Configuring Basic Spring Application

---
+++ Week2_SpringCore_Maven/Exercise1_LibraryManagement/src/main/java/org/example/App.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import com.library.repository.BookRepository;
+import com.library.service.BookService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        BookRepository repository =
+                context.getBean("bookRepository", BookRepository.class);
+
+        BookService service =
+                context.getBean("bookService", BookService.class);
+
+        repository.displayRepository();
+        service.displayService();
+    }
+}
