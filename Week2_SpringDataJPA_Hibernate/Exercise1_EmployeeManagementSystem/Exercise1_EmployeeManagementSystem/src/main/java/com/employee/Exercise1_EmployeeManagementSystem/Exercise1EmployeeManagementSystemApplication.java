a03414d8e082e28b39895fe18847e178e25dae98
sandhyas2308 <23104140@rmd.ac.in>
2026-07-02 14:08:04 +0530

    Added Exercise 1 - Employee Management System Setup

---
+++ Week2_SpringDataJPA_Hibernate/Exercise1_EmployeeManagementSystem/Exercise1_EmployeeManagementSystem/src/main/java/com/employee/Exercise1_EmployeeManagementSystem/Exercise1EmployeeManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.employee.Exercise1_EmployeeManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise1EmployeeManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise1EmployeeManagementSystemApplication.class, args);
+	}
+
+}
---
+++ Week2_SpringDataJPA_Hibernate/Exercise1_EmployeeManagementSystem/Exercise1_EmployeeManagementSystem/src/test/java/com/employee/Exercise1_EmployeeManagementSystem/Exercise1EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee.Exercise1_EmployeeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1EmployeeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
